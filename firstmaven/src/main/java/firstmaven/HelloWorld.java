277d8a6c073f71862e0bc541caf9777310b334f8
Divya Teja <divyagovardhan.kandula@gmail.com>
2016-10-10 13:24:27 -0500

    second chage added in Helloworld.java file by Teja

--- firstmaven/src/main/java/firstmaven/HelloWorld.java
+++ firstmaven/src/main/java/firstmaven/HelloWorld.java
@@ -1,10 +1,16 @@
 package firstmaven;
 
+
 public class HelloWorld {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-  System.out.println("hello world");
+
+
+
+
+System.out.println("second change");
+System.out.println("hello world");
 		
 		
 		System.out.println("Hi Teja, This is from global update");
