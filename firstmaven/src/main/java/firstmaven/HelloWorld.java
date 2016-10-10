7f517ad1433bb09b622571471aa605b539a18184
divyatej26 <divyagovardhan.kandula@gmail.com>
2016-10-10 13:15:38 -0500

    Update HelloWorld.java

--- firstmaven/src/main/java/firstmaven/HelloWorld.java
+++ firstmaven/src/main/java/firstmaven/HelloWorld.java
@@ -5,5 +5,8 @@ public class HelloWorld {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
   System.out.println("hello world");
+		
+		
+		System.out.println("Hi Teja, This is from global update");
 	}
 }
